09f83bdfea93cdbc10ae98e12f39cdbccf8d7909
nakashimmer <3724063+nakashimmer@users.noreply.github.com>
2019-09-21 15:40:04 +0900

    up

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,74 @@
+const CACHE_NAME = 'cache-v1';
+const urlsToCache = [
+    './',
+    './apple-touch-icon.png',
+    './canvas,js',
+    './html5-192.png',
+		'./html5-256.png',
+		'./index.html'
+];
+
 self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-	msg.innerHTML="インストールしました";
+   event.waitUntil(
+        caches.open(CACHE_NAME)
+              .then((cache) => {
+                  console.log('Opened cache');
+
+                  // 指定されたリソースをキャッシュに追加する
+                  return cache.addAll(urlsToCache);
+              })
+    );
 });
 
 self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
-	msg.innerHTML="アクティベートしました";
+   var cacheWhitelist = [CACHE_NAME];
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    // ホワイトリストにないキャッシュ(古いキャッシュ)は削除する
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
 
 // 現状では、この処理を書かないとService Workerが有効と判定されないようです
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+        caches.match(event.request)
+              .then((response) => {
+                  if (response) {
+                      return response;
+                  }
+
+                  // 重要：リクエストを clone する。リクエストは Stream なので
+                  // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
+                  // 必要なので、リクエストは clone しないといけない
+                  let fetchRequest = event.request.clone();
+
+                  return fetch(fetchRequest)
+                      .then((response) => {
+                          if (!response || response.status !== 200 || response.type !== 'basic') {
+                              return response;
+                          }
+
+                          // 重要：レスポンスを clone する。レスポンスは Stream で
+                          // ブラウザ用とキャッシュ用の2回必要。なので clone して
+                          // 2つの Stream があるようにする
+                          let responseToCache = response.clone();
+
+                          caches.open(CACHE_NAME)
+                                .then((cache) => {
+                                    cache.put(event.request, responseToCache);
+                                });
+
+                          return response;
+                      });
+              })
+    );
+});
