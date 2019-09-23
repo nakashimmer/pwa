6f3980c80c593bc326999ea0c571a346dc64c150
nakashimmer <nakacma@gmail.com>
2019-09-23 20:50:02 +0900

    きのうにもどす

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,75 @@
-const CACHE_NAME = 'cache-001';
+const CACHE_NAME = 'cache-v2';
 const urlsToCache = [
-		'/',
     './apple-touch-icon.png',
     './canvas.js',
     './html5-192.png',
 		'./html5-256.png',
 		'./index.html',
-		'./service-worker.js',
-		'./manifest.json'
+		'./service-worker.js'
 ];
 
-// インストール処理
-self.addEventListener('install', function (event) {
-	event.waitUntil(
-		caches
-			.open(CACHE_NAME)
-			.then(function (cache) {
-				return cache.addAll(
-					urlsToCache.map(
-						url => new Request(
-							url,{ credentials: 'same-origin' }
-						)
-					)
-				)
-			})
-	);
+self.addEventListener('install', (event)=>{
+   event.waitUntil(
+        caches.open(CACHE_NAME)
+              .then((cache) => {
+                  console.log('Opened cache');
+
+                  // 指定されたリソースをキャッシュに追加する
+                 return cache.addAll(urlsToCache.map(url => new Request(url, {credentials: 'same-origin'})));
+
+              })
+    );
 });
 
-// リソースフェッチ時のキャッシュロード処理
-self.addEventListener('fetch', function (event) {
+self.addEventListener('activate', (event)=>{
+   var cacheWhitelist = [CACHE_NAME];
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    // ホワイトリストにないキャッシュ(古いキャッシュ)は削除する
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// 現状では、この処理を書かないとService Workerが有効と判定されないようです
+self.addEventListener('fetch', (event)=>{
 	event.respondWith(
-		caches
-			.match(event.request)
-			.then(function (response) {
-				return response ? response : fetch(event.request);
-			})
-	);
+        caches.match(event.request)
+              .then((response) => {
+                  if (response) {
+                      return response;
+                  }
+
+                  // 重要：リクエストを clone する。リクエストは Stream なので
+                  // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
+                  // 必要なので、リクエストは clone しないといけない
+                  let fetchRequest = event.request.clone();
+
+                  return fetch(fetchRequest)
+                      .then((response) => {
+                          if (!response || response.status !== 200 || response.type !== 'basic') {
+                              return response;
+                          }
+
+                          // 重要：レスポンスを clone する。レスポンスは Stream で
+                          // ブラウザ用とキャッシュ用の2回必要。なので clone して
+                          // 2つの Stream があるようにする
+                          let responseToCache = response.clone();
+
+                          caches.open(CACHE_NAME)
+                                .then((cache) => {
+                                    cache.put(event.request, responseToCache);
+                                });
+
+                          return response;
+                      });
+              })
+    );
 });
